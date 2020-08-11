37019e9f1a33a550ddbf9cbe2a6b97ecbd0570b4
Carlo Nucera <carlo.nucera@protonmail.com>
2020-08-11 11:06:52 -0400

    Created a repo for the reif pack

---
+++ pack.pl
@@ -0,0 +1,5 @@
+name(reif).
+title('Reified if, reification library').
+version('1.0.0').
+author('Ulrich Neumerkel', 'ulrich.neumerkel@tuwien.ac.at').
+maintainer('Carlo Nucera', 'meditans@gmail.com').
